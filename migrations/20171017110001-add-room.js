2ed2accc2cfdea0113c46ee74e8581f127d0cfe1
kevinng88 <gstcollection@hotmail.com>
2017-10-20 02:20:07 +0800

    kevin: fix merge

--- migrations/20171017110001-add-room.js
+++ migrations/20171017110001-add-room.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 'use strict';
 module.exports = {
   up: (queryInterface, Sequelize) => {
@@ -10,17 +9,3 @@ module.exports = {
     return queryInterface.removeColumn('rooms','DJ_room');
   }
 };
-||||||| merged common ancestors
-=======
-'use strict';
-module.exports = {
-  up: (queryInterface, Sequelize) => {
-    return queryInterface.addColumn('rooms', 'DJ_room', {
-        type: Sequelize.STRING
-      })
-  },
-  down: (queryInterface, Sequelize) => {
-    return queryInterface.removeColumn('rooms','DJ_room');
-  }
-};
->>>>>>> d31f022c3c89a2fca1fb78c0db5d34d828bd1411
